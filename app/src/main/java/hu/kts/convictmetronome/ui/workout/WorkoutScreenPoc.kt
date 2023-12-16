29a549df9c8cdafab74388e3eccddcc74e0c3408
Andras Nemeth <andras.nemethhh@gmail.com>
2023-12-16 16:08:43 +0100

    poc long click

--- app/src/main/java/hu/kts/convictmetronome/ui/workout/WorkoutScreenPoc.kt
+++ app/src/main/java/hu/kts/convictmetronome/ui/workout/WorkoutScreenPoc.kt
@@ -1,14 +1,18 @@
 package hu.kts.convictmetronome.ui.workout
 
+import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,14 +24,19 @@ import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hu.kts.convictmetronome.ui.theme.ConvictMetronomeTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WorkoutScreenPoc() {
     val transition = rememberInfiniteTransition(label = "shimmer")
+    val haptic = LocalHapticFeedback.current
     val progressAnimated by transition.animateFloat(
         initialValue = 0f,
         targetValue = 1f,
@@ -69,7 +78,20 @@ fun WorkoutScreenPoc() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("0", fontSize = 128.sp)
+            Text("0",
+                fontSize = 128.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .combinedClickable(
+                        onClick = {Log.d("xxx", "short") },
+                        onLongClick = {
+                            haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                            Log.d("xxx", "long")
+                        },
+                    )
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
