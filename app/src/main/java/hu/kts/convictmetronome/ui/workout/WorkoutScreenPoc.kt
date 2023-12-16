e6f93e9f0bc72688d74a16c4b2c16053bbbf2f06
Andras Nemeth <andras.nemethhh@gmail.com>
2023-12-16 15:29:54 +0100

    poc workout animation

---
+++ app/src/main/java/hu/kts/convictmetronome/ui/workout/WorkoutScreenPoc.kt
@@ -0,0 +1,83 @@
+package hu.kts.convictmetronome.ui.workout
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hu.kts.convictmetronome.ui.theme.ConvictMetronomeTheme
+
+@Composable
+fun WorkoutScreenPoc() {
+    val transition = rememberInfiniteTransition(label = "shimmer")
+    val progressAnimated by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = "shimmer"
+    )
+    Surface (
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .drawWithCache {
+                val progress = 0.5f
+                val gradientThickness = 0.1f
+                val offset = progress * (1 + gradientThickness)
+
+                
+                val brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.White,
+                        Color(0xFF42A5F5)
+                    ),
+                    startY = size.height * (offset - gradientThickness) ,
+                    endY = size.height * offset
+                )
+                onDrawBehind {
+                    drawRoundRect(
+                        brush,
+                        cornerRadius = CornerRadius(10.dp.toPx())
+                    )
+                }
+            },
+        color = Color.Transparent
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("0", fontSize = 128.sp)
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PreviewWorkoutScreen() {
+    ConvictMetronomeTheme {
+        WorkoutScreenPoc()
+    }
+}
