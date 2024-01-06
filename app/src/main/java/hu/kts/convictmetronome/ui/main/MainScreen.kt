e937f066c3de2c16c89563d018c90319ca57c5aa
Andras Nemeth <andras.nemethhh@gmail.com>
2024-01-06 19:55:39 +0100

    volume popup design finalization

--- app/src/main/java/hu/kts/convictmetronome/ui/main/MainScreen.kt
+++ app/src/main/java/hu/kts/convictmetronome/ui/main/MainScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.MoreVert
@@ -30,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import hu.kts.convictmetronome.R
 import hu.kts.convictmetronome.core.Sounds
 import hu.kts.convictmetronome.ui.theme.CmTheme
@@ -151,13 +154,19 @@ private fun VolumePopup(
         onDismissRequest = actionCallbacks::dismissVolumePopup,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Text(stringResource(id = R.string.app_bar_volume_explanation))
-        Slider(
-            value = value,
-            onValueChange = actionCallbacks::onVolumeChange,
-            valueRange = 0f..Sounds.maxVolume,
-            steps = 4
-        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(id = R.string.app_bar_volume_explanation),
+                color = MaterialTheme.colorScheme.outline,
+            )
+            Slider(
+                value = value,
+                onValueChange = actionCallbacks::onVolumeChange,
+                valueRange = 0f..Sounds.maxVolume,
+                steps = 4,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            )
+        }
     }
 }
 
