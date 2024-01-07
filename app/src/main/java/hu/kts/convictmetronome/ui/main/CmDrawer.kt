0c9e306a38929df16e2ba894269ccef85a4910de
Andras Nemeth <andras.nemethhh@gmail.com>
2024-01-07 17:53:24 +0100

    add icon to drawer header

--- app/src/main/java/hu/kts/convictmetronome/ui/main/CmDrawer.kt
+++ app/src/main/java/hu/kts/convictmetronome/ui/main/CmDrawer.kt
@@ -1,10 +1,12 @@
 package hu.kts.convictmetronome.ui.main
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -13,8 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,14 +65,25 @@ private fun DrawerContent(
     coroutineScope: CoroutineScope,
 ) {
     ModalDrawerSheet {
-        Text(
-            text = stringResource(id = R.string.app_name),
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.headlineSmall,
-            modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 16.dp),
-            fontWeight = FontWeight.Bold
-        )
-        Divider()
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 16.dp)
+        ) {
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_metronome),
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(start = 8.dp),
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Divider(modifier = Modifier.padding(top = 8.dp, bottom = 8.dp))
         exercises.forEach { exercise ->
             NavigationDrawerItem(
                 label = { Text(text = exercise.name) },
