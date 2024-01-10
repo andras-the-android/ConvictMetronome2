9b04e59656dd8cac54df5c7a432641b582f74ebe
Andras Nemeth <andras.nemethhh@gmail.com>
2024-01-10 08:46:12 +0100

    update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "hu.kts.cmetronome"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 14
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
